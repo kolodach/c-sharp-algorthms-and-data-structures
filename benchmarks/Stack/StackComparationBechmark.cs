08c60c4a5ec366d6201a3fa7362da52d19ab2e72
Kolodach <kolodach41@gmail.com>
2018-09-02 10:43:40 +0300

    Added statck pop benchmark

--- benchmarks/Stack/StackComparationBechmark.cs
+++ benchmarks/Stack/StackComparationBechmark.cs
@@ -8,25 +8,33 @@ namespace Benchmark
     {
         public StackCompationBenchmark()
         {
-            PushTest(new SinglyLinkedList<int>(), 1000);
-            PushTest(new Stack<int>(), 1000);
-            PushTest(new DefaultStackWrapper<int>(), 1000);
-            System.Console.WriteLine("------------------------------------");
-            PushTest(new SinglyLinkedList<int>(), 1_0_000);
-            PushTest(new Stack<int>(), 1_0_000);
-            PushTest(new DefaultStackWrapper<int>(), 1_0_000);
-            System.Console.WriteLine("------------------------------------");
-            PushTest(new SinglyLinkedList<int>(), 1_00_000);
-            PushTest(new Stack<int>(), 1_00_000);
-            PushTest(new DefaultStackWrapper<int>(), 1_00_000);
-            System.Console.WriteLine("------------------------------------");
-            PushTest(new SinglyLinkedList<int>(), 1_000_000);
-            PushTest(new Stack<int>(), 1_000_000);
-            PushTest(new DefaultStackWrapper<int>(), 1_000_000);
-            System.Console.WriteLine("------------------------------------");
-            PushTest(new SinglyLinkedList<int>(), 100_000_000);
-            PushTest(new Stack<int>(), 100_000_000);
-            PushTest(new DefaultStackWrapper<int>(), 100_000_000);
+            for (int i = 1000; i <= 100_000_000; i *= 10)
+            {
+                PushTest(new SinglyLinkedList<int>(), i);
+                PushTest(new Stack<int>(), i);
+                PushTest(new DefaultStackWrapper<int>(), i);
+                System.Console.WriteLine("------------------------------------");
+            }
+
+            for (int i = 1000; i <= 100_000_000; i *= 10)
+            {
+                PopTest(new SinglyLinkedList<int>(), i);
+                PopTest(new Stack<int>(), i);
+                PopTest(new DefaultStackWrapper<int>(), i);
+                System.Console.WriteLine("------------------------------------");
+            }
+        }
+
+        public void PopTest(IStack<int> stack, int count)
+        {
+            System.Console.WriteLine($"{stack.ToString()}: Push {count} elements ");
+            for (int i = 0; i < count; i++)
+                stack.Push(i);
+            System.Console.WriteLine($"{stack.ToString()}: Pop {count} elements ");            
+            var startTime = DateTime.Now;
+             for (int i = 0; i < count; i++)
+                stack.Pop();
+            System.Console.WriteLine($"Time total: {(DateTime.Now - startTime).TotalMilliseconds}");
         }
 
         private void PushTest(IStack<int> stack, int count)
